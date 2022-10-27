3bb05e7828036b2e6bc3bb03dcc9196cc8adbdca
KantTurnsInHisGrave <112070854+KantTurnsInHisGrave@users.noreply.github.com>
2022-10-27 14:15:20 +0100

    Update build.sh

--- Flask Web App/build.sh
+++ Flask Web App/build.sh
@@ -2,7 +2,7 @@
 
 echo "building app..."
 
-cd /home/jenkins/.jenkins/workspace/DFE_final_project_pipeline/FlasWebApp
+cd /home/jenkins/.jenkins/workspace/DFE_final_project_pipeline/website
 
 sudo chown "$USER":"$USER" /var/run/docker.sock -R
 sudo chmod g+rwx "/var/run/docker.sock" -R
